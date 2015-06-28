8635ff71e86e94726b444155327363290f1d7549
Miek Gieben <miek@miek.nl>
2015-06-28 11:42:56 +0100

    var-args: fix example: use correct function name
    This could never compile.
    
    Fixes #11

--- ex/functions/src/var-arg.go
+++ ex/functions/src/var-arg.go
@@ -3,11 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	prtthem(1, 4, 5, 7, 4)
-	prtthem(1, 2, 4)
+	printthem(1, 4, 5, 7, 4)
+	printthem(1, 2, 4)
 }
 
-func printtthem(numbers ...int) {
+func printthem(numbers ...int) {
 	for _, d := range numbers {
 		fmt.Printf("%d\n", d)
 	}
